b54243e8d7b790766ec57a7423f0ea2eccb480d2
Givaild <gil_nm@hotmail.com>
2022-11-23 09:38:31 -0300

    feat:create ands style header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
 import todoLogo from "../../assets/todoLogo.svg";
+import {AiOutlinePlusCircle} from 'react-icons/ai';
+import styles from './header.module.css'
 
 export function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={todoLogo} alt="todo logo" />
+      <form action=""className={styles.newTaskForm}>
+        <input type="text" placeholder="Adicione uma nova tarefa"/>
+        <button>
+          Criar
+        <AiOutlinePlusCircle size={20}/>
+        </button>
+      </form>
     </header>
   );
 }
